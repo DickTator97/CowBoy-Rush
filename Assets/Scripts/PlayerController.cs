c312e65f83843f938742861a7a2420017b4638a5
DickTator97 <hors245@gmail.com>
2023-06-25 16:36:18 +0300

    Added Some insignificent changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+      //movment
+      //1. move player forward with add force
+      //2. move player sideways with touch input
+      //3. make player jump while dragging finger upwards
+      //4. make player slide while dragging finger downwards
+      
+    // score
+    //life 
+    //animations (if exisiting)
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,5 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }

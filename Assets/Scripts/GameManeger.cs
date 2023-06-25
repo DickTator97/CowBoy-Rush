20bacd02a88bf4e19cf83f749a80a28784378892
DickTator97 <hors245@gmail.com>
2023-06-25 19:35:11 +0300

    Added Some Changes To The Game ,Added WIP UI And Some BackGround Music

---
+++ Assets/Scripts/GameManeger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+    #region UI 
+    #region Main Menu
+    public void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+    }
+    #endregion
+    #endregion
+}

0df0282b1d180b5797766da8b0fdb0f75b109d04
sahersaeed1212 <sahersa2@hotmail.com>
2023-07-03 04:50:31 +0200

    Revert "wtf"
    This reverts commit 288399216eb691d78f7877b19be9f8f0d8fd5843, reversing
    changes made to adf76c4072f3605c0a13972d50da301cb29829da.

--- Assets/Scripts/Enviornment/Map_Boundary.cs
+++ Assets/Scripts/Enviornment/Map_Boundary.cs
@@ -3,34 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map_Boundary : MonoBehaviour
+public class MapBoundary : MonoBehaviour
 {
 
     [SerializeField] static float left_bound;
     [SerializeField] static float right_bound;
     public float internal_left;
     public float internal_right;
-
-    private void Start()
-    {
-    }
     void Update()
     {
-        AreBordersValid();
         internal_left = left_bound;
         internal_right = right_bound;
         
     }
   
-    void AreBordersValid()
-    {
-        if (internal_left>-11)
-        {
-            internal_left =-11;
-        }
-        if (internal_right<11)
-        {
-            internal_right = 11;
-        }
-    }
 }

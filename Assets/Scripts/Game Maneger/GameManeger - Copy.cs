61c9e0cfb5e8c57572b8923fb98b8147c26f0278
sahersaeed1212 <saeedsaher59@gmail.com>
2023-07-03 14:51:15 +0200

    come onnn

---
+++ Assets/Scripts/Game Maneger/GameManeger - Copy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+   
+    #region UI 
+    #region Main Menu
+    public void OnMouseDown()
+    {
+     
+        Debug.Log("Clicked");
+    }
+    #endregion
+    #endregion
+}
